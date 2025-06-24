bb8770734458ace0984f4da24e4d59cbda40b308
Emmanuel Moulin <78955048+Kibishi47@users.noreply.github.com>
2025-06-24 20:26:23 +0200

    feat: static site on vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
 import tailwindcss from "@tailwindcss/vite";
+import vercelStatic from '@astrojs/vercel/static';
 
 export default defineConfig({
   integrations: [svelte()],
   output: 'static',
+  adapter: vercelStatic(),
   vite: {
     plugins: [tailwindcss()],
     resolve: {
